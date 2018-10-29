390c49e7f7b3665890ed2c199b8fd58baebc2d28
Davis <davish225@gmail.com>
2018-10-29 15:33:40 -0400

    Added loop to go through and find all input files needed

--- runTestSuite.sh
+++ runTestSuite.sh
@@ -10,3 +10,21 @@ $(jar cfe frontendtest.jar Interface *)
 cd ../
 
 java -jar bin/frontendtest.jar
+
+cd ./"Tests"
+
+file=0
+
+for d in */Input.txt ;
+
+do
+  if [ -f "$d" ] ; then
+    file=$((file+1))
+	echo $d
+  fi
+done
+
+
+
+
+echo "files $file"
