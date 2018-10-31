fd567822a9be50b8c831358435a3e31986006be4
dunstu <15ds66@queensu.ca>
2018-10-30 22:11:16 -0400

    Improves test harness looping functionality

--- runTestSuite.sh
+++ runTestSuite.sh
@@ -14,29 +14,27 @@ cd ../
 
 #java -jar bin/frontendtest.jar
 
-cd ./Testing/Input_tests
+cd ./Testing
 
 
-#for d in Input_Tests/*/ ;
-#do
-#testname=${d#*/}
-testname="Cancelticket - correct service number as agent"
-input="./Input_Tests/$testname/Input.txt"
-expected="./Expected_Tests/$testname/Output.txt"
-output="./Output_Tests/$testname/Output.txt"
-echo "" > testSummary.txt
+for d in Input_Tests/*/ ;
+do
+    testname=${d#*/}
+    input="./Input_Tests/$testname/Input.txt"
+    expected="./Expected_Tests/$testname/Output.txt"
+    output="./Output_Tests/$testname/Output.txt"
+    echo "" > testSummary.txt
 
-echo 1
-java -jar ../bin/"$exe" < "$input" > "templog.txt"
-echo 2
-cat "transactionSummary.txt" > "$output"
-echo 3
-diff "$expected" "$output" > "/dev/null"
+    echo $testname
 
-if (( $? == 0 )); then
-    echo "Pass : $testname" >> testSummary.txt
-else
-    echo "Fail : $testname" >> testSummary.txt
-fi
+    java -jar ../bin/"$exe" < "$input" > "templog.txt"
+    cat "transactionSummary.txt" > "$output"
+    diff "$expected" "$output" > "/dev/null"
+
+    if (( $? == 0 )); then
+        echo "Pass : $testname" >> testSummary.txt
+    else
+        echo "Fail : $testname" >> testSummary.txt
+    fi
  
-#done
+done
