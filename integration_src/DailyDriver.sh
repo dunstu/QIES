354ea7716566abeeaae9a43bfcc3aeed15dff805
dunstu <15ds66@queensu.ca>
2018-11-25 19:06:01 -0500

    merge upstream

--- integration_src/DailyDriver.sh
+++ integration_src/DailyDriver.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
+# Program takes one integer argument: the day of the week [1, 5]
+day="$1"
 
 scriptroot=$(dirname "$(readlink -f "$0")")
 cd $scriptroot/..
 qiesroot=$PWD
+mts=$qiesroot/IntegrationWorkspace/transactionSummary.txt
 
 # Compile front office
-frontexe=frontend.jar
+frontname=frontend.jar
 cd $qiesroot/front_src
 $(javac -d ../bin Interface.java Parser.java Database.java)
 cd $qiesroot/bin 
-$(jar cfe ${frontexe} Interface *)
+$(jar cfe ${frontname} Interface *)
 rm *.class
-cd $qiesroot
+frontexe=${qiesroot}/bin/${frontname}
 
 # Compile back office
-backexe=backend.jar
+backname=backend.jar
 cd $qiesroot/back_src
 $(javac -d ../bin BackParser.java BackDatabase.java Service.java)
 cd $qiesroot/bin
-$(jar cfe ${backexe} BackParser BackParser.class BackDatabase.class Service.class)
+$(jar cfe ${backname} BackParser BackParser.class BackDatabase.class Service.class)
 rm *.class
-cd $qiesroot
-
+backexe=${qiesroot}/bin/${backname}
 
 # For each console
-
+for d in $qiesroot/IntegrationWorkspace/console*; do
+    echo $d
     # run front end with input in ./input
-
-    # Append output to ../mergedTransactionSummary 
+    cd $d
+    echo $PWD
+    java -jar $frontexe < ./inputs/day${day} > "/dev/null" 
+    cat "transactionSummary.txt" >> $mts
+done
 
 # Run back office    
 
